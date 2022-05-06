613fa7f18ae578f95a5c949eed5b4ca28ff453d6
boybenson <ybenson96@gmail.com>
2022-05-06 09:41:25 +0000

    feat: minimum number guessing game

--- guess-a-number.py
+++ guess-a-number.py
@@ -1,2 +1,14 @@
+import random as rand
+
 print("Welcome To The Best Number Guessing Game On The Internet!")
 
+while True:
+    guessedNumber = input("Please Guess A Number_")
+    secretNumber = rand.randint(1, 5)
+
+    if int(guessedNumber) != secretNumber:
+        print(f"sorry,😅 The correct number is {secretNumber}")
+        continue
+    else:
+        print("Congratulations!!")
+        quit()
